1a85b6afd4be6d23eb55a37b0480a86e8a59580f
Thiago Seus <thiago.seus@yahoo.com.br>
2021-03-10 15:36:43 -0300

    Adicionar serviço do cliente

---
+++ src/main/java/br/com/zup/loja/services/ClientesService.java
@@ -0,0 +1,40 @@
+package br.com.zup.loja.services;
+
+import br.com.zup.loja.exceptions.CPFRepetidoException;
+import br.com.zup.loja.exceptions.ClienteNaoEncontradoException;
+import br.com.zup.loja.exceptions.ProdutoNaoEncontradoException;
+import br.com.zup.loja.models.Cliente;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ClientesService {
+    private List<Cliente> clientes;
+
+    public ClientesService() {
+        clientes = new ArrayList<>();
+    }
+
+    public Cliente adicionarCliente(Cliente cliente) {
+        try {
+            pesquisarClientePeloCPF(cliente.getCpf());
+        } catch (ClienteNaoEncontradoException erro) {
+            clientes.add(cliente);
+            return cliente;
+        }
+
+        throw new CPFRepetidoException();
+    }
+
+    public Cliente pesquisarClientePeloCPF(String cpf) {
+        for (Cliente cliente: clientes) {
+            if (cliente.getCpf().equals(cpf)) {
+                return cliente;
+            }
+        }
+
+        throw new ProdutoNaoEncontradoException();
+    }
+}
